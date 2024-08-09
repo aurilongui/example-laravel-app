2ac4093e6d3ad01f82acc61b72decc5b011cf996
aurora <aurora.lara@externo.dominioncommercial.es>
2024-08-09 09:36:38 +0200

    Editing, updating and deleting jobs

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ Route::get('/', function () {
     return view('home');
 });
 
+//Index
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->latest() ->simplePaginate(3); //paginate() te añade número de página
     return view('jobs.index', [
@@ -15,15 +16,18 @@ Route::get('/jobs', function () {
     ]);
 });
 
+//Create
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
 
+//Show
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
     return view('jobs.show', ['job' => $job]);
 });
 
+//Store
 Route::post('/jobs', function () {
     request()->validate([
         'title'=>['required', 'min:3'],
@@ -37,6 +41,40 @@ Route::post('/jobs', function () {
     ]);
 
     return redirect('/jobs');
+});
+
+//Edit
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+    return view('jobs.edit', ['job' => $job]);
+});
+
+//Update
+Route::patch('/jobs/{id}', function ($id) {
+    request()->validate([
+        'title'=>['required', 'min:3'],
+        'salary'=>['required']
+    ]);
+
+        //authorize (On hold...)
+
+    $job = Job::findOrFail($id);
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+
+    return redirect('/jobs/' . $job->id);
+});
+
+//Destroy
+Route::delete('/jobs/{id}', function ($id) {
+     //authorize (On hold...)
+
+     Job::findOrFail($id)->delete();
+
+     return redirect('/jobs');
 });
 
 Route::get('/contact', function () {
