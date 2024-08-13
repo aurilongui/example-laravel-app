7c36f833dbeeb55a34c82fc1fdd5cbb3929e84f4
aurora <aurora.lara@externo.dominioncommercial.es>
2024-08-13 09:46:02 +0200

    getting the build process in order

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My website</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite(['resources/js/app.js'])
 </head>
 
 <body class="h-full">
@@ -33,7 +33,7 @@
                         @auth
                             <form method="POST" action="/logout">
                                 @csrf
-                                
+
                                 <x-form-button>Log Out</x-form-button>
                             </form>
                         @endauth
